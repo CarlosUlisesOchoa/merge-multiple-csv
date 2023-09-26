183e82493ec5be33a357ab74b0d333db5abc2649
Carlos Ochoa <uligibson@hotmail.com>
2023-09-26 13:33:56 -0600

    this version merges two csv, unique rows and only specific columns.

--- main.py
+++ main.py
@@ -1,43 +1,41 @@
 from datetime import datetime
 
-def read_csv_file(file_path):
-    with open(file_path, 'r') as f:
-        return f.readlines()
-
-def write_csv_file(file_path, lines):
-    with open(file_path, 'w') as f:
-        f.writelines(lines)
-
 def process_line(line):
     line = line.strip()
     cols = line.split(",")
-    
+
     if len(cols) < 23:
-        return None
-    
+        return None, None
+
     try:
         date_str = cols[5].replace('"', '').split(" ")[0]
         date_obj = datetime.strptime(date_str, '%m/%d/%y')
+        
+        # Adjust year to be in the 20th century
+        if date_obj.year >= 2000:
+            date_obj = date_obj.replace(year=date_obj.year - 100)
+        
         formatted_date = date_obj.strftime('%d/%m/%Y')
     except:
         formatted_date = ""
-    
-    # Exclude columns "s,l,mza,consec,cred,folio,nac" by slicing
-    new_cols = cols[:5] + [f'"{formatted_date}"'] + cols[6:13] + [cols[18]] + [cols[-1]]
-    return ",".join(new_cols) + "\n"
+
+    # Include "l" and "s" (indexed as 15 and 16) while skipping the "edad" (indexed as 1) and second-to-last columns (indexed as -2)
+    new_cols = [cols[0]] + cols[2:5] + [f'"{formatted_date}"'] + cols[6:15] + cols[15:17] + [cols[-1]]
+    return cols[0].replace('"', ''), ",".join(new_cols) + "\n"
 
 def main():
-    csv1_lines = read_csv_file("1.csv")
-    csv2_lines = read_csv_file("2.csv")
-    
-    processed_lines = set()
-    for line in csv1_lines[1:] + csv2_lines[1:]:
-        processed_line = process_line(line)
-        if processed_line:
-            processed_lines.add(processed_line)
-    
-    processed_lines = sorted(list(processed_lines))
-    write_csv_file("merged.csv", ["cve,edad,nombre,paterno,materno,fecnac,sexo,calle,int,ext,colonia,cp,e,d,m,cred,curp\n"] + processed_lines)
+    seen_cves = set()
+    with open("merged.csv", 'w') as f_out:
+        f_out.write("clave_elector,nombre,apellido_paterno,apellido_materno,fecha_nacimiento,sexo,calle,numero_interior,numero_exterior,colonia,codigo_postal,id_estado,d,numero_municipio,seccion,localidad,curp\n")
+        for file_name in ["1.csv", "2.csv"]:
+            with open(file_name, 'r') as f_in:
+                # Skip headers
+                next(f_in)
+                for line in f_in:
+                    cve, processed_line = process_line(line)
+                    if processed_line and cve not in seen_cves:
+                        seen_cves.add(cve)
+                        f_out.write(processed_line)
 
 if __name__ == "__main__":
     main()
